aeb020fa5d6d0329a584f6b8e5faf2aaeb22955b
cooler-SAI <cooler19860212@yahoo.com>
2016-05-01 16:59:45 +0300

    fix Evade State

--- Development/Locations/Eastern Kingdoms/Ghostlands.sql
+++ Development/Locations/Eastern Kingdoms/Ghostlands.sql
@@ -148,7 +148,8 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,0,0,3400,4700,11,9613,64,0,0,0,0,2,0,0,0,0,0,0,0,'Cast bolt'),
 (@ENTRY,0,1,0,0,0,100,0,5000,8000,15700,23400,11,18267,0,0,0,0,0,5,0,0,0,0,0,0,0,'Cast Curse of Weakness'),
-(@ENTRY,0,2,0,1,0,100,1,1000,1000,1000,1000,11,51733,2,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Shadow Aura on Spawn');
+(@ENTRY,0,2,0,1,0,100,1,1000,1000,1000,1000,11,51733,2,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Shadow Aura on Spawn'),
+(@ENTRY,0,3,0,7,0,100,1,0,0,0,0,11,51733,2,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Shadow Aura on Evade');
 
 -- NPC talk text insert
 SET @ENTRY := 16318;
